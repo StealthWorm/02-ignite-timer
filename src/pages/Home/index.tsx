3316aed451ed721920bc56f8d868bf477071a285
StealthWorm <thierrypitela@hotmail.com>
2023-08-04 22:13:23 -0300

    feat: add react-hook-form control

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -10,16 +13,59 @@ import {
   TaskInput,
 } from './styles'
 
+// ? controlled x  uncontrolled
+// ? no controlled sempre mantemos o estado de um componente atualizado, em tempo real. A cada nova modificação, atualziamos o estado (formularios simples)
+// ! interfaces longas e complexas podem  sofrer impacto com essa abordagem, uma vez que cada componente será renderizado novamente
+// ? no uncontrolled, buscamos o valor somente quando precisarmos dele. Perde em fluidez, mas ganha em performance (dashboards grandes)
+
+/* caso queira verificar mensagens de erro através do zod e hookforms ao inves de utilizar o HTML direto,
+   basta usar a funcionaidade "formState" do useForm, dentro dela tem uma prop chamada "formState.errors"
+*/
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  owner: zod.string(),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 60 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
+
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+// * para referenciar uma var Javascript dentro do Typescript usamos o "typeof"
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -36,6 +82,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +96,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
